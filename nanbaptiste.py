e62ebfaad34dff6580c29a169a9a76c16a279313
Antoine Coulon <antoine.coulon@epita.fr>
2020-02-02 06:05:00 +0100

    sad

--- nanbaptiste.py
+++ nanbaptiste.py
@@ -1,8 +1,5 @@
 import RPi.GPIO as GPIO
 import time
-import pyaudio
-import wave
-import os
 import subprocess
 
 def main():
@@ -15,29 +12,7 @@ def main():
     while True:
         r = GPIO.input(BUTTON1_PIN)
         if r == False:
-            print("bouton pressax")
-            playsound("sounds/nanbaptiste.wav")
             subprocess.run("play sounds/nanbaptiste.wav".split())
-
-
-def playsound(path):
-    chunk = 1024
-    f = wave.open( path,"rb")
-    p = pyaudio.PyAudio()
-    stream = p.open(format = p.get_format_from_width(f.getsampwidth()),
-                    channels = f.getnchannels(),
-                    rate = f.getframerate(),
-                    output = True)
-
-    data = f.readframes(chunk)
-    while data:
-        stream.write(data)
-        data = f.readframes(chunk)
-
-    stream.stop_stream()
-    stream.close()
-
-    p.terminate()
 
 
 main()
