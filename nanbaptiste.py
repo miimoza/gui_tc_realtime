e28955818d31c984e72623c22e1da18d6be7ef11
Antoine Coulon <antoine.coulon@epita.fr>
2020-02-07 20:55:23 +0100

    asd

--- nanbaptiste.py
+++ nanbaptiste.py
@@ -8,17 +8,22 @@ def main():
     GPIO.setwarnings(False)
     GPIO.setmode(GPIO.BCM)
 
-    thread_button_1 = Thread(target = button_check, args = (18,))
+
+    thread_button_1 = Thread(target = button_check, args = (18,"sounds/nanbaptiste.wav"))
+    thread_button_2 = Thread(target = button_check, args = (14,"sounds/tuvoispas.mp3"))
+    thread_button_3 = Thread(target = button_check, args = (15,"sounds/jsuistresnet.mp3"))
 
     thread_button_1.start()
+    thread_button_2.start()
+    thread_button_3.start()
 
-def button_check(gpio_number):
+def button_check(gpio_number, sound_path):
     GPIO.setup(gpio_number, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
     while True:
         r = GPIO.input(gpio_number)
         if r == False:
             print("nan baptiste")
-            cmd = "play -q sounds/nanbaptiste.wav".split()
-            Thread(target = subprocess.run, args = (cmd,)).start()
+            cmd = "play -q " + sound_path
+            Thread(target = subprocess.run, args = (cmd.split(),)).start()
             time.sleep(0.5)
