63b07ac3b0886505d5d6a22fa9123aed7e9d5386
Antoine Coulon <antoine.coulon@epita.fr>
2020-05-22 22:20:32 +0200

    qsd

--- button_wrapper.py
+++ button_wrapper.py
@@ -10,8 +10,8 @@ def main():
     GPIO.setmode(GPIO.BCM)
 
     thread_button_1 = Thread(target = play_random_baptiste, args = (18))
-    thread_button_2 = Thread(target = button_check, args = (14,"sounds/tuvoispas.mp3"))
-    thread_button_3 = Thread(target = button_check, args = (15,"sounds/jsuistresnet.mp3"))
+    thread_button_2 = Thread(target = print_news, args = (14))
+    thread_button_3 = Thread(target = print_news, args = (15))
 
     thread_button_1.start()
     thread_button_2.start()
@@ -33,12 +33,5 @@ def play_random_baptiste(gpio_number):
             time.sleep(0.5)
 
 def print_news(gpio_number):
-    GPIO.setup(gpio_number, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
-    while True:
-        r = GPIO.input(gpio_number)
-        if r == False:
-            cmd = "play -q " + sound_path + " -t alsa"
-
-            Thread(target = subprocess.run, args = (cmd.split(),)).start()
-            time.sleep(0.5)
+    os.system('clear')
+    print("cool ces news dis donc..")
